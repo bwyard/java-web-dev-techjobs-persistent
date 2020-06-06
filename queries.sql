85bfc54001119fbc3d8238d25b6d4a1af6dee93f
bwyard <bwyard@gmail.com>
2020-06-06 11:04:06 -0500

    part 2 sql

--- queries.sql
+++ queries.sql
@@ -4,6 +4,7 @@ employer (varchar(255))
 name (varchar(255))
 skills (varchar(255))
 ## Part 2: Test it with SQL
+SELECT name FROM techjobs.employer WHERE location = "St. Louis City";
 
 ## Part 3: Test it with SQL
 
