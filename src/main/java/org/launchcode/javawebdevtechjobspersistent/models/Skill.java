8760171dd63b7d4987837c562dd2214e2e2f0367
bwyard <bwyard@gmail.com>
2020-06-08 18:18:37 -0500

    updated list,home,and skill

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -3,13 +3,28 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 import com.sun.istack.NotNull;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs;
+
     @NotNull
     private String description;
 
     public Skill() {
+    }
+
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
     }
 
     public String getDescription() {
