5d0bdbf65d39e758428081ce2b1756c72a5288a4
bwyard <bwyard@gmail.com>
2020-06-08 18:17:22 -0500

    updated sql

--- queries.sql
+++ queries.sql
@@ -7,5 +7,7 @@ skills (varchar(255))
 SELECT name FROM techjobs.employer WHERE location = "St. Louis City";
 
 ## Part 3: Test it with SQL
+DROP TABLE techjobs.job;
 
 ## Part 4: Test it with SQL
+SELECT name, description FROM skill INNER JOIN job_skill ON skill.id = job_skill.job_id WHERE description IS NOT NULL ORDER BY name ASC;
